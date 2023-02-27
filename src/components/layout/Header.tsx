aa0a80419a6d1d813236844bb5e04880fa3d22e8
ptabakovendigital <126526837+ptabakovendigital@users.noreply.github.com>
2023-02-28 01:19:51 +0200

    Add files via upload

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,1 +1,14 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
 
+export default function Header() {
+
+    return (
+        <AppBar position='static'>
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Typography variant="h6">
+                    Stock Exchange Demo App
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    )
+}
