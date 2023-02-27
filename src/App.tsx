75d5cb4ac9898ebc90c5b52838e49ea916f5a782
ptabakovendigital <126526837+ptabakovendigital@users.noreply.github.com>
2023-02-28 01:17:13 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,17 @@
+import React from 'react';
+import { Container } from "@mui/material";
+import Header from "./components/layout/Header";
+import CompanyStatistics from "./components/CompanyStatistics";
 
+function App() {
+  return (
+    <div className="App">
+      <Header />      
+      <Container sx={{mt: 4}}>
+        <CompanyStatistics />
+      </Container>      
+    </div>
+  );
+}
+
+export default App;
